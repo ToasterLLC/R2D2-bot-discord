40f519edee8de4247947ea011c908d22d1414d55
ToasterLLC <lalwaniaditya4@gmail.com>
2021-07-17 13:45:33 +0530

    Added help command

---
+++ commands/help.js
@@ -0,0 +1,19 @@
+const config = require("../config.json");
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "help",
+    description: "Shows the available commands.",
+    execute(message, args, client){
+        const embed = new Discord.MessageEmbed()
+            .setColor("#ffffff")
+            .setTitle("These are the available commands.")
+            .addFields(
+                {"name":"Prefix", "value": `${config.prefix}`},
+                {"name":"ping", "value": "Shows the ping."},
+                {"name": "rng", "value": "Outputs a number from 0-n or x-y depending on 1 or 2 numbers are input."}
+            )
+
+          message.channel.send(embed);
+    }
+}
