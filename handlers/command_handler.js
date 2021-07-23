39391a47797e9a226821f86c95cc344b6f1d6a92
ToasterLLC <lalwaniaditya4@gmail.com>
2021-07-23 16:40:40 +0530

    File organisation

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -1,13 +1,16 @@
 const fs = require("fs");
 
 module.exports = (client, Discord) => {
-    const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
-    for (const file of commandFiles) {
-        const command = require(`../commands/${file}`);
-        if (command.name) {
-            client.commands.set(command.name, command);
-        } else {
-            continue;
+    const loadDir = (Dirs) => {
+            const commandFiles = fs.readdirSync(`./commands/${Dirs}`).filter(file => file.endsWith(".js"));
+            for (const file of commandFiles) {
+                const command = require(`../commands/${Dirs}/${file}`);
+                if (command.name) {
+                    client.commands.set(command.name, command);
+                } else {
+                    continue;
+                }
+            }
         }
-    }
+        ["core", "mod", "dev", "math"].forEach(e => loadDir(e));
 }
--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const db = require("mongoose");
 require("dotenv").config();
 client.commands = new Discord.Collection();
 
 ["command_handler", "event_handler"].forEach((handler)=>{
     require(`./handlers/${handler}`)(client, Discord);
 })
-
-// db.connect(process.env.MONGO_SRV, {
-//     useNewUrlParser : true,
-//     useUnifiedTopology : true,
-//     useFindAndModify: false
-// }).then(()=>{
-//     console.log("Database connected.");
-// }).catch(err => {
-//    console.log(err)
-// })
 
 client.login(process.env.TOKEN);
